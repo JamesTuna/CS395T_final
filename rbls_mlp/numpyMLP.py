6ba5e82591fcf3d00a00ee791a2de999acd0420c
JamesTuna <dzhuocheng@gmail.com>
2020-11-11 15:25:55 -0600

    rbls regressed mlp: no obvious robustness improvement

--- rbls_mlp/numpyMLP.py
+++ rbls_mlp/numpyMLP.py
@@ -9,12 +9,15 @@ sys.path.insert(1,'../mlp')
 from cMLP import *
 from trainer import *
 from rbls import *
+from numpy.random import normal
 
 class npMLP():
-    def __init__(self,cMLP_model_path):
-        st_dict = torch.load(cMLP_model_path)
+    def __init__(self,cMLP_model_path=None):
         self.Ws = []
         self.bs = []
+        if cMLP_model_path is None:
+            return
+        st_dict = torch.load(cMLP_model_path)
         print("loading saved model: %s"%cMLP_model_path)
         for name in st_dict:
             print(name)
@@ -29,16 +32,43 @@ class npMLP():
         x[x<0] = 0
         return x
 
-    def forward_n(self,x,n):
+    def forward_nlayer(self,x,n,noise=0):
         # x shape: [n samples, 784]
         # return the outputs of n th layer(no activation of nth layer)
-        assert n>=0 and n<=len(self.Ws), 'layer index out of range'
+        assert n>=0 and n<=len(self.Ws), 'layer index out of range, should be in range [0,%s]'%(len(self.Ws))
         if n>0:
             for i in range(0,n-1):
-                x = x.dot(self.Ws[i]) + self.bs[i]
+                x = x.dot(self.Ws[i]*normal(1,noise,self.Ws[i].shape)) + self.bs[i]*normal(1,noise,self.bs[i].shape)
                 x = self.act(x)
-            x = x.dot(self.Ws[n-1]) + self.bs[n-1]
+            x = x.dot(self.Ws[n-1]*normal(1,noise,self.Ws[n-1].shape)) + self.bs[n-1]*normal(1,noise,self.bs[n-1].shape)
         return x
+
+
+    def estimate_input_nlayer(self,x,n,noise,samples=100):
+        # estimate mean and var of A matrix in rbls, shape [#samples, #feature]
+        assert n>=1 and n<=len(self.Ws), 'layer index out of range, should be in range [1,%s]'%(len(self.Ws))
+
+        sample_inputs = []
+
+
+        for i in range(samples):
+            noisy_out = self.forward_nlayer(x,n-1,noise=noise)
+            noisy_out = self.act(noisy_out) # [n samples, out_dim]
+            sample_inputs.append(noisy_out)
+
+        sample_inputs = np.array(sample_inputs) # [100, #samples, #features]
+
+        mean = sample_inputs.mean(axis=0)
+        var = sample_inputs.var(axis=0)
+
+        return mean, var
+
+
+
+
+
+
+
 
 
 
@@ -67,16 +97,34 @@ if __name__ == "__main__":
     y_test_onehot = np.load('../rbls/data/y_test_onehot.npy') # [10000,10]
     y_test = np.load('../rbls/data/y_test.npy') #[10000,1]
 
-    # debug: check correctness of npMLP.forward_n
+    # debug: check correctness of function forward_nlayer() and estimate_input()
     np_model = npMLP("l2h32noise0n1_lr0.1ep40decay10rate0.1.ckpt")
     device='cuda:0' if torch.cuda.is_available() else 'cpu'
     torch_model = cMLP(input_dim=28*28,output_dim=10,num_layers=2,num_hidden_neurons=32).to(device)
     torch_model.load_state_dict(torch.load("l2h32noise0n1_lr0.1ep40decay10rate0.1.ckpt"))
     # random sample a batch of inputs
     x = A_train[:5,:]
-    np_out = np_model.forward_n(x,2)
+    np_out = np_model.forward_nlayer(x,1,noise=0)
     torch_out = torch_model(torch.from_numpy(x).to(device))
     print("np")
     print(np_out)
     print("true")
     print(torch_out)
+    # estimate mean input variance of nth layer
+    print("estimate input mean and variance for layer 2")
+    mean,var = np_model.estimate_input_nlayer(x,n=2,noise=0.1,samples=1000)
+    print('mean')
+    print(mean)
+    print('var')
+    print(var)
+    # estimate_input_nlayer() time on whole MNIST dataset,sample=1000
+    start = time.time()
+    mean,var = np_model.estimate_input_nlayer(A_train,n=2,noise=0.5,samples=100)
+    end = time.time()
+    print("%.4f seconds elasped "%(end-start))
+    # check if it's reasonable to estimate var using a small fraction of x
+    print("var on whole train: %.4f",var.mean())
+    for i in range(10):
+        choosen_samples = np.random.choice(A_train.shape[0],1000)
+        var_ = var[choosen_samples]
+        print("var on randomly choosen 1000 samples: %.4f"%var_.mean())
