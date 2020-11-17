eaf1ef85e8f8d5d2a90dc9688ff4fecf6a7ed166
Zihao James <dzuocheng@gmail.com>
2020-11-16 23:12:37 -0600

    test scripts for vortex models

--- vortex/testVortex.py
+++ vortex/testVortex.py
@@ -3,6 +3,7 @@
 import torch.nn as nn
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
 
 class VortexLinear(nn.Module):
     def __init__(self,in_dim,out_dim,init_std=0.1):
@@ -90,6 +91,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--load', type=str, default=None, help='resume training from saved model')
     parser.add_argument('--noise',type=float, default=0,help='noise std in test')
+    parser.add_argument('--logdir',type=str, default=0,help='where to save tested results')
+    parser.add_argument('--samples',type=int, default=2000,help='how many samples of noises')
     args = parser.parse_args()
 
     A_test = np.load('../rbls/data/A_test.npy') # [10000,784]
@@ -97,7 +100,7 @@ if __name__ == "__main__":
 
     # test
     W = np.load(args.load)
-    noise_sample=2000
+    noise_sample=args.samples
     accuracy = []
     print("test under noise %.4f"%(args.noise))
     for test in range(noise_sample):
@@ -110,3 +113,29 @@ if __name__ == "__main__":
         accuracy.append(acc)
     accuracy = np.array(accuracy)
     print("avg acc %.4f"%(accuracy.mean()))
+
+    # statistics
+    acc_list = accuracy
+    np.save(args.logdir+'/acc.npy',acc_list)
+    with open(args.logdir+'/statistics.txt','w') as f:
+        for q in range(1,20):
+            qtl = 0.05 * q
+            qtl_acc = np.quantile(acc_list,qtl)
+            f.write('%.3f quantile:\tacc: %.4f\t\n'%(qtl,qtl_acc))
+        f.write('Expectation:\tacc: %.4f\n'%(acc_list.mean()))
+        f.write('Variance:\tacc: %.4f'%(acc_list.var()))
+    # histogram
+    #ax3 = plt.subplot(3,1,3)
+    num_bins = 10
+    acc_mean = acc_list.mean()
+    heights,bins = np.histogram(acc_list,num_bins)
+    freq = heights/sum(heights)
+    width = (max(bins)-min(bins))/(len(bins))
+    plt.bar(bins[:-1]+1/2*width,freq,width=width,color='blue',alpha=0.5)
+    plt.xticks(np.around(bins,decimals=3))
+    plt.title('Accuracy distribution of %s perturbations with mean %s'%(acc_list.shape[0],np.around(acc_mean,decimals=4)))
+    plt.xlabel('Accuracy')
+    plt.ylabel('Frequency')
+    plt.rc('grid', linestyle="-", color='black')
+    plt.grid()
+    plt.savefig(args.logdir+'/test_%s_perturbation_%s.pdf'%(args.samples,args.noise))
